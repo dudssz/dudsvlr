e3d87e207906546b03855bfbc64a8cf93fc11dcb
dudssz <164358737+dudssz@users.noreply.github.com>
2024-04-04 00:28:27 -0300

    Add files via upload

---
+++ js/badges.js
@@ -0,0 +1,34 @@
+// tippy("#developer", {
+//     content: "Developer",
+//     animation: 'scale'
+//     });
+    
+    tippy("#hypesquad", {
+    content: "HypeSquad Balance",
+    animation: 'scale'
+    });
+    
+    tippy("#nitro", {
+    content: "Assinante Nitro",
+    animation: 'scale'
+    });
+    
+    tippy("#nitroboost", {
+    content: "Assinante Boost",
+    animation: 'scale'
+    });
+      
+    tippy("#verified", {
+    animation: 'scale',
+    content: "Verificado"
+    });
+    
+    tippy("#oldname", {
+    animation: 'scale',
+    content: "Originally known as duds#7105"
+    });
+
+    tippy("#developer", {
+        content: "Developer",
+        animation: 'scale'
+        });
